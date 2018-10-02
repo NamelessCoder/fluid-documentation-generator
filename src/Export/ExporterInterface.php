46a91d9f0872c18e3acb1f81d6ecd5875b36bccd
Anja Leichsenring <anja.leichsenring@typo3.com>
2018-10-02 10:54:43 +0200

    [BUGFIX] Pass the forceUpdate parameter into the Exporter

--- src/Export/ExporterInterface.php
+++ src/Export/ExporterInterface.php
@@ -3,9 +3,9 @@ declare(strict_types=1);
 
 namespace NamelessCoder\FluidDocumentationGenerator\Export;
 
-use NamelessCoder\FluidDocumentationGenerator\ProcessedSchema;
 use NamelessCoder\FluidDocumentationGenerator\Entity\SchemaPackage;
 use NamelessCoder\FluidDocumentationGenerator\Entity\SchemaVendor;
+use NamelessCoder\FluidDocumentationGenerator\ProcessedSchema;
 use NamelessCoder\FluidDocumentationGenerator\SchemaDocumentationGenerator;
 use NamelessCoder\FluidDocumentationGenerator\ViewHelperDocumentation;
 use NamelessCoder\FluidDocumentationGenerator\ViewHelperDocumentationGroup;
@@ -14,7 +14,8 @@ interface ExporterInterface
 {
     public function getIdentifier(): string;
     public function setGenerator(SchemaDocumentationGenerator $generator): void;
-    public function exportRoot(): void;
+
+    public function exportRoot(bool $forceUpdate): void;
     public function exportVendor(SchemaVendor $vendor): void;
     public function exportPackage(SchemaPackage $package): void;
     public function exportSchema(ProcessedSchema $processedSchema, bool $forceUpdate = false): void;
